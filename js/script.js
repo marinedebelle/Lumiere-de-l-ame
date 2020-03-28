0ceed4562de486012bac782afcd765c3eed42a44
marinedebelle <marinedebelle@outlook.com>
2020-03-28 21:18:42 +0100

    ajout des jauges interactives

--- js/script.js
+++ js/script.js
@@ -1,56 +1,151 @@
-/* Attendre le chargement du DOM */
-document.addEventListener( 'DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
 
+  /* 
+  Declarations
+  */
+  //
 
+  /* 
+  Functions
+  */
+      const getFeelingValue = (titleTag, title, listItem, emotionRange) => {
+          // Set title
+          titleTag.textContent = title;
 
+          // Loop on list item
+          for( let i = 0; i < listItem.length; i++ ){
+              // Get item
+              const item = listItem[i];
 
+              // Get click event on eeach item
+              item.addEventListener('click', () => {
+                  // Set feeling
+                  feelingActivation(title, listItem, emotionRange, i);
+              })
+          }
+      }
 
-  
-      //JAUGES
-      var max = 10, // Set max value
-    initvalue = 1, // Set the initial value
-    icon = "fa-fire", // Set the icon (https://fontawesome.com/icons)
-    target = document.querySelectorAll('[data-value]'),
-    listIcon = document.getElementById("labels-list");
+      const feelingActivation = (title, listItem, emotionRange, index) => {
+          // Loop on list item to delete class
+          for( let i = 0; i < listItem.length; i++ ){
+              // Remove class active
+              listItem[i].classList.remove('active');
+          }
 
-  // Function to update du value
+          // Loop on list item to add class
+          for( let i = 0; i <= index; i++ ){
+              // Remove class active
+              listItem[i].classList.add('active');
+          }
 
-  function updateValue(target, value){
-    target.forEach(function(currentIndex) {
-      currentIndex.dataset.value =  value;
-    });
-  }
+          // Edit emotion range size
+          let rangeSize = (index + 1) * 10;
+          emotionRange.style.width = rangeSize + '%';
 
-  // Init the number of item with the initial value settings
+          // Set emotion range
+          localStorage.setItem( title, index );
+      }
+  //
 
-  for (i = 0; i < max; i++) { 
-    var picto = "<i class='fas "+ icon +"'></i>";
-    $(".labels").append(picto);
-  }
+  /* 
+  Start interface
+  */
+      getFeelingValue(
+          document.querySelector('#joy h2'), 
+          'joie',
+          document.querySelectorAll('#joy li'), 
+          document.querySelector('#joy .emotionRange')
+      );
 
-  updateValue(target, initvalue);
+      getFeelingValue(
+          document.querySelector('#excitation h2'), 
+          'excitation',
+          document.querySelectorAll('#excitation li'), 
+          document.querySelector('#excitation .emotionRange')
+      );
 
-  // Update the slider on click
+      getFeelingValue(
+        document.querySelector('#fun h2'), 
+        'amusement',
+        document.querySelectorAll('#fun li'), 
+        document.querySelector('#fun .emotionRange')
+      );
 
-  $('.fas').on( "click", function(){
-    var index = $(this).index() + 1;
-    $( "#range-slider" ).slider( "value", index );
-    updateValue(target, index);
-  });
+      getFeelingValue(
+      document.querySelector('#anger h2'), 
+      'colère',
+      document.querySelectorAll('#anger li'), 
+      document.querySelector('#anger .emotionRange')
+      );
 
+      getFeelingValue(
+        document.querySelector('#desire h2'), 
+        'désir',
+        document.querySelectorAll('#desire li'), 
+        document.querySelector('#desire .emotionRange')
+      );
 
-  // Init the slider
+      getFeelingValue(
+        document.querySelector('#uncomfortable h2'), 
+        'embarras',
+        document.querySelectorAll('#uncomfortable li'), 
+        document.querySelector('#uncomfortable .emotionRange')
+      );
 
-  $( "#range-slider" ).slider({
-    range: "min",
-    value: initvalue,
-    min: 1, 
-    max: max, 
+      getFeelingValue(
+        document.querySelector('#boredom h2'), 
+        'ennui',
+        document.querySelectorAll('#boredom li'), 
+        document.querySelector('#boredom .emotionRange')
+      );
 
-    slide: function( event, ui ) {                     
-      updateValue(target, ui.value);
-    }
-  });
+      getFeelingValue(
+        document.querySelector('#serenity h2'), 
+        'sérénité',
+        document.querySelectorAll('#serenity li'), 
+        document.querySelector('#serenity .emotionRange')
+      );
 
+      getFeelingValue(
+        document.querySelector('#nostalgia h2'), 
+        'nostalgie',
+        document.querySelectorAll('#nostalgia li'), 
+        document.querySelector('#nostalgia .emotionRange')
+      );
 
-});
+      getFeelingValue(
+        document.querySelector('#fear h2'), 
+        'peur',
+        document.querySelectorAll('#fear li'), 
+        document.querySelector('#fear .emotionRange')
+      );
+
+      getFeelingValue(
+        document.querySelector('#anxiety h2'), 
+        'anxiété',
+        document.querySelectorAll('#anxiety li'), 
+        document.querySelector('#anxiety .emotionRange')
+      );
+
+      getFeelingValue(
+        document.querySelector('#sadness h2'), 
+        'tristesse',
+        document.querySelectorAll('#sadness li'), 
+        document.querySelector('#sadness .emotionRange')
+      );
+
+      getFeelingValue(
+        document.querySelector('#satisfaction h2'), 
+        'satisfaction',
+        document.querySelectorAll('#satisfaction li'), 
+        document.querySelector('#satisfaction .emotionRange')
+      );
+
+      getFeelingValue(
+        document.querySelector('#disgust h2'), 
+        'dégoût',
+        document.querySelectorAll('#disgust li'), 
+        document.querySelector('#disgust .emotionRange')
+      );
+  //
+})
